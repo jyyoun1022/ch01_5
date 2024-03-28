7e4f2b3bc56be178f6f61422add89b1c5c88638d
jyyoun1022 <jyyoun1022@naver.com>
2024-03-29 01:11:31 +0900

    use ArrowComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import * as D from './data'
 import ClassComponent from './ClassComponent'
+import ArrowComponent from './ArrowComponent'
 
 export default function App() {
   return (
     <ul>
       <ClassComponent href="https://google.com" text="go to Google" />
-      <ClassComponent href="https://twitter.com" text="go To Twitter" />
+      <ArrowComponent href="https://twitter.com" text="go To Twitter" />
     </ul>
   )
 }
--- src/ClassComponent.tsx
+++ src/ClassComponent.tsx
@@ -1,20 +1,18 @@
 import {Component} from 'react'
 
 type ClassComponentProps = {
-  href: string,
-  text: string,
+  href: string
+  text: string
 }
 export default class App extends Component<ClassComponentProps> {
   render() {
     const {href, text} = this.props
     return (
-      <ul>
-        <li>
-          <a href={href}>
-            <p>{text}</p>
-          </a>
-        </li>
-      </ul>
+      <li>
+        <a href={href}>
+          <p>{text}</p>
+        </a>
+      </li>
     )
   }
 }
